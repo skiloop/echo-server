da83ba5684e04eed007f9b026788f7e7b39ba6e0
skiloop <skiloop@gmail.com>
2025-05-08 23:23:57 +0800

    feat: add support for TLS versions in JA3 hash generation

--- ja3/ja3.go
+++ ja3/ja3.go
@@ -14,6 +14,7 @@ type Ja3 struct {
 	Extensions []uint16 `json:"extensions"`
 	Curves     []uint16 `json:"curves"`
 	Points     []uint16 `json:"points"`
+	Versions   []uint16 `json:"versions"`
 }
 
 func (j *Ja3) Md5Hash() string {
@@ -24,6 +25,7 @@ func (j *Ja3) Md5Hash() string {
 		ja3 = append(ja3, getList(j.Extensions))
 		ja3 = append(ja3, getList(j.Curves))
 		ja3 = append(ja3, getList(j.Points))
+		ja3 = append(ja3, getList(j.Versions))
 		s := strings.Join(ja3, ",")
 		j.Hash = utils.GetMD5Hash(s)
 		fmt.Printf("ja3: %s, raw ja3: %s\n", j.Hash, s)
@@ -43,7 +45,11 @@ func GenJA3Raw(info tls.ClientHelloInfo) (Ja3, error) {
 	var ja3 Ja3
 	ja3.Version = getMaxVersion(info)
 	ja3.Ciphers = info.CipherSuites
-	ja3.Extensions = getExtensions(info)
+	//ja3.Extensions = getExtensions(info)
+	ja3.Extensions = make([]uint16, len(info.Extensions))
+	copy(ja3.Extensions, info.Extensions)
+	ja3.Versions = make([]uint16, len(info.SupportedVersions))
+	copy(ja3.Versions, info.SupportedVersions)
 	ja3.setCurves(info)
 	ja3.setPoints(info)
 	return ja3, nil
@@ -74,16 +80,47 @@ func getMaxVersion(info tls.ClientHelloInfo) uint16 {
 
 func getExtensions(info tls.ClientHelloInfo) []uint16 {
 	var data []uint16
+
+	// SNI (Server Name Indication)
 	if info.ServerName != "" {
-		data = append(data, 0)
+		data = append(data, 0) // Extension ID for SNI
 	}
+
+	// Supported Points
 	if len(info.SupportedPoints) > 0 {
-		data = append(data, 11)
+		data = append(data, 11) // Extension ID for EC Point Formats
 	}
-	// TODO: supported groups
+
+	// Supported Curves
 	if len(info.SupportedCurves) > 0 {
-		data = append(data, 10)
+		data = append(data, 10) // Extension ID for Supported Groups
 	}
+
+	// ALPN
+	if len(info.SupportedProtos) > 0 {
+		data = append(data, 16) // Extension ID for ALPN
+	}
+
+	// Signature Algorithms
+	if len(info.SignatureSchemes) > 0 {
+		data = append(data, 13) // Extension ID for Signature Algorithms
+	}
+
+	// Supported Versions
+	if len(info.SupportedVersions) > 0 {
+		data = append(data, 43) // Extension ID for Supported Versions
+	}
+	//
+	//// PSK (TLS 1.3)
+	//if len(info.PskIdentities) > 0 {
+	//	data = append(data, 45) // Extension ID for PSK
+	//}
+
+	//// Key Share (TLS 1.3)
+	//if len(info.KeyShares) > 0 {
+	//	data = append(data, 51) // Extension ID for Key Share
+	//}
+
 	return data
 }
 func getList(x interface{}) string {
